62bbd092a2950933f8103c7336be46e4a2d9a88d
Вини <yvinivitin@gmail.com>
2022-12-10 17:39:40 +0200

    vite compression test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    viteCompression({
+      filter: `/\.(js|mjs|json|css|html)$/i`,
+      verbose: true,
+      threshold: 1025,
+      algorithm: `gzip`,
+      ext: `.gz`,
+    }),
+  ],
 });
